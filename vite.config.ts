fe49e8c739bd775b4fbcea4df8161811ef2fcae1
Morten Esbensen <1204080+mortenesbensen@users.noreply.github.com>
2025-04-16 22:43:50 +0200

    Adding vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+const base = process.env.NODE_ENV === 'production' 
+  ? '/yin-flow-landing-page/' 
+  : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -14,6 +18,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: base,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
