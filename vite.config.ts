57cd6132446c0480a36a689f3fa6e3ab5a2e057f
Morten Esbensen <1204080+mortenesbensen@users.noreply.github.com>
2025-04-16 22:53:47 +0200

    Revert

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const base = process.env.NODE_ENV === 'production' 
-  ? '/yin-flow-landing-page/' 
-  : '/'
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -18,7 +14,6 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: base,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
