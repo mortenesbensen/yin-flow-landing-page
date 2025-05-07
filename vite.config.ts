65cd19ec52ec5b49067897d8e9e12233fcadf6f4
Morten Esbensen <1204080+mortenesbensen@users.noreply.github.com>
2025-05-07 22:44:44 +0200

    Add vite to basepath

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const base = process.env.BASE_PATH || "/";
+const base = process.env.VITE_BASE_PATH || "/";
 
 export default defineConfig(({ mode }) => ({
   server: {
