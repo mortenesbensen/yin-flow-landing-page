cc7c8b4659e2da120b37649dda2ebf1f2462b496
Morten Esbensen <1204080+mortenesbensen@users.noreply.github.com>
2025-05-07 23:02:11 +0200

    Attempt fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { config } from "dotenv";
+
+config();
 
 const base = process.env.VITE_BASE_PATH || "/";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  base: base,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  console.log("VITE_BASE_PATH", base);
+  return {
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+      Boolean
+    ),
+    base: base,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
